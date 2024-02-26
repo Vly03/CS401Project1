875e9b14f35226ed1892c77a68070c72ad9de062
Victor <victorly0033@gmail.com>
2024-02-25 22:03:16 -0800

    AddressBook - Added createEntry() as a helper function for addEntry()

--- src/address/data/AddressBook.java
+++ src/address/data/AddressBook.java
@@ -1,14 +1,36 @@
 package address.data;
-import java.util.ArrayList;
+import address.Menu;
 
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AddressBook
 {
     private ArrayList<AddressEntry> entryList;
+    public static Scanner userInput = new Scanner(System.in);
+    public static String userString;
+    public static int userInt;
 
 
     public AddressBook()        { entryList = new ArrayList<>();}
 
+
+    public void createEntry()
+    {
+        AddressEntry newEntry = new AddressEntry("tempFirstName", "", "", "", "", "",
+                "", "");
+
+        newEntry.setFirstName(Menu.prompt_FirstName());
+        newEntry.setLastName(Menu.prompt_LastName());
+        newEntry.setStreet(Menu.prompt_Street());
+        newEntry.setCity(Menu.prompt_City());
+        newEntry.setState(Menu.prompt_State());
+        newEntry.setZip(Menu.prompt_Zip());
+        newEntry.setTelephone(Menu.prompt_Telephone());
+        newEntry.setEmail(Menu.prompt_Email());
+
+        entryList.add(newEntry);
+    }
 
     public void addEntry(AddressEntry entry)
     {
@@ -29,5 +51,6 @@ public class AddressBook
             System.out.println(entry);
         }
     }
+
 }
 
