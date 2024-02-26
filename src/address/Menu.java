1af2ae19464a6d5da0a4385f95aad68b0dfe4029
Victor <victorly0033@gmail.com>
2024-02-25 22:01:41 -0800

    Menu - Added displayMenu(), ifContainsInt(), ifContainsLetters()

--- src/address/Menu.java
+++ src/address/Menu.java
@@ -1,7 +1,13 @@
 package address;
+import java.io.*;
+import java.util.Scanner;
 
 public class Menu
 {
+    public static Scanner userInput = new Scanner(System.in);
+    public static String userString;
+    public static int userInt;
+
     public static void displayMenu()
     {
         System.out.println("++++++++++++++++++++++++++++++++++++++++++++++");
@@ -17,16 +23,127 @@ public class Menu
         System.out.println("++++++++++++++++++++++++++++++++++++++++++++++");
     }
 
-    public static String prompt_FirstName()     {return "First Name:";}
+    public static boolean ifContainsInt(String str)
+    {
+        for (int i = 0; i < str.length(); i++ )
+        {
+            if (str.charAt(i) >= 48 && str.charAt(i) <= 57)
+                return true;
+        }
+        return false;
+    }
 
-    public static String prompt_LastName()      {return "Last Name:";}
-    public static String prompt_Street()        {return "Street:";}
+    public static boolean ifContainsLetters(String str)
+    {
+        for (int i = 0; i < str.length(); i++ )
+        {
+            if (str.charAt(i) < 48 || str.charAt(i) > 57)
+                return true;
+        }
+        return false;
+    }
 
-    public static String prompt_City()          {return "City: ";}
-    public static String prompt_State()         {return "State: ";}
-    public static String prompt_Zip()           {return "Zip: ";}
-    public static String prompt_Telephone()     {return "Telephone: ";}
-    public static String prompt_Email()         {return "Email:";}
+    public static String prompt_FirstName()
+    {
+        boolean invalidInput;
+
+        System.out.println("First name: ");
+        userString = userInput.nextLine();
+
+        if (ifContainsInt(userString))
+        {
+            System.out.println("Invalid! Contains an integer. Try again");
+            prompt_FirstName();
+        }
+        return userString;
+    }
+    public static String prompt_LastName()
+    {
+        boolean invalidInput;
+
+        System.out.println("Last name: ");
+        userString = userInput.nextLine();
+
+        if (ifContainsInt(userString))
+        {
+            System.out.println("Invalid! Contains an integer. Try again");
+            prompt_LastName();
+        }
+        return userString;
+    }
+    public static String prompt_Street()
+    {
+        System.out.println("Street: ");
+        userString = userInput.nextLine();
+        return userString;
+    }
+
+    public static String prompt_City()
+    {
+        boolean invalidInput;
+
+        System.out.println("City: ");
+        userString = userInput.nextLine();
+
+        if (ifContainsInt(userString))
+        {
+            System.out.println("Invalid! Contains an integer. Try again");
+            prompt_City();
+        }
+        return userString;
+    }
+
+    public static String prompt_State()
+    {
+        boolean invalidInput;
+
+        System.out.println("State: ");
+        userString = userInput.nextLine();
+
+        if (ifContainsInt(userString))
+        {
+            System.out.println("Invalid! Contains an integer. Try again");
+            prompt_State();
+        }
+        return userString;
+    }
+
+    public static String prompt_Zip()
+    {
+        boolean invalidInput;
+
+        System.out.println("Zip: ");
+        userString = userInput.nextLine();
+
+        if (ifContainsLetters(userString))
+        {
+            System.out.println("Invalid! Contains a letter. Try again");
+            prompt_Zip();
+        }
+        return userString;
+    }
+
+    public static String prompt_Telephone()
+    {
+        boolean invalidInput;
+
+        System.out.println("Telephone: ");
+        userString = userInput.nextLine();
+
+        if (ifContainsLetters(userString))
+        {
+            System.out.println("Invalid! Contains a letter. Try again");
+            prompt_Telephone();
+        }
+        return userString;
+    }
+
+    public static String prompt_Email()
+    {
+        System.out.println("Email: ");
+        userString = userInput.nextLine();
+        return userString;
+    }
 }
 
 
