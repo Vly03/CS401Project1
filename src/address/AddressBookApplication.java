caab8a5e8dda440d7c6cae83e3e10316c054b367
Victor <victorly0033@gmail.com>
2024-02-25 22:04:13 -0800

    Initialized menu selection loop (1-6)

--- src/address/AddressBookApplication.java
+++ src/address/AddressBookApplication.java
@@ -3,6 +3,7 @@ package address;
 
 import address.data.AddressBook;
 import address.data.AddressEntry;
+import java.util.Scanner;
 
 
 public class AddressBookApplication
@@ -15,11 +16,49 @@ public class AddressBookApplication
     }
 
 
-    public static void main(String args[])
+    public static void main(String[] args)
     {
-        Menu.displayMenu();
+        Scanner userInput = new Scanner(System.in);
+        int selection = 0;
         AddressBook ab = new AddressBook();
+        String userString;
+        int userInt;
 
+
+
+        while (selection != 6)
+        {
+            Menu.displayMenu();
+            selection = userInput.nextInt();
+            userInput.nextLine();
+
+            if (selection == 1)
+            {
+            }
+
+            if (selection == 2)
+            {
+                ab.createEntry();
+            }
+
+            if (selection == 3)
+            {
+            }
+
+            if (selection == 4)
+            {
+            }
+
+            if (selection == 5)
+            {
+            }
+
+            if (selection == 6)
+            {
+                System.out.println("Exiting the program.");
+            }
+
+        }
     }
 }
 
