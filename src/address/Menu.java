b93049219bfa3465441791ef1e602c847f1b0384
Victor <victorly0033@gmail.com>
2024-02-25 22:37:46 -0800

    Menu - Added user input correctness to prompt_firstName(), prompt_lastName(),prompt_city(), prompt_street(), prompt_state(), prompt_zip(), prompt_telephone()

--- src/address/Menu.java
+++ src/address/Menu.java
@@ -2,12 +2,22 @@ package address;
 import java.io.*;
 import java.util.Scanner;
 
+/**
+ * Menu functions as a pseudo helper class to AddressBookApplication that generates and displays the console that the user sees
+ * @author Victor Ly
+ * @since 1.0 Exercise 2
+ */
 public class Menu
 {
     public static Scanner userInput = new Scanner(System.in);
     public static String userString;
     public static int userInt;
 
+    /**
+     * Displays Options (1-6) to the user
+     * @author Victor Ly
+     * @since (2/24) Commit 2640e6d19b84517aebc3361d89bfebe59119e433
+     */
     public static void displayMenu()
     {
         System.out.println("++++++++++++++++++++++++++++++++++++++++++++++");
@@ -23,6 +33,13 @@ public class Menu
         System.out.println("++++++++++++++++++++++++++++++++++++++++++++++");
     }
 
+    /**
+     * Helper function for user input correctness that determines if a string contains an integer
+     * @author Victor Ly
+     * @param str String from user input
+     * @return boolean True -> Contains integer, False --> No integer
+     * @since (2/25) Commit 1af2ae19464a6d5da0a4385f95aad68b0dfe4029
+     */
     public static boolean ifContainsInt(String str)
     {
         for (int i = 0; i < str.length(); i++ )
@@ -33,6 +50,13 @@ public class Menu
         return false;
     }
 
+    /**
+     * Helper function for user input correctness that determines if a string contains a letter
+     * @author Victor Ly
+     * @param str String from user input
+     * @return True -> Contains a letter, False --> No letters
+     * @since (2/25) Commit 1af2ae19464a6d5da0a4385f95aad68b0dfe4029
+     */
     public static boolean ifContainsLetters(String str)
     {
         for (int i = 0; i < str.length(); i++ )
@@ -43,6 +67,12 @@ public class Menu
         return false;
     }
 
+    /**
+     * Prompts user for a first name, updated with user input correctness i.e. no numbers allowed in a name
+     * @author Victor Ly
+     * @return String firstName
+     * @since (2/25) Commit 1af2ae19464a6d5da0a4385f95aad68b0dfe4029
+     */
     public static String prompt_FirstName()
     {
         boolean invalidInput;
@@ -57,6 +87,13 @@ public class Menu
         }
         return userString;
     }
+
+    /**
+     * Prompts user for a last name, updated with user input correctness i.e. no numbers allowed in a name
+     * @author Victor Ly
+     * @return String lastName
+     * @since (2/25) Commit 1af2ae19464a6d5da0a4385f95aad68b0dfe4029
+     */
     public static String prompt_LastName()
     {
         boolean invalidInput;
@@ -71,6 +108,13 @@ public class Menu
         }
         return userString;
     }
+
+    /**
+     * Prompts user for a street, both letters and numbers are allowed in a street
+     * @author Victor Ly
+     * @return String street
+     * @since (2/25) Commit 1af2ae19464a6d5da0a4385f95aad68b0dfe4029
+     */
     public static String prompt_Street()
     {
         System.out.println("Street: ");
@@ -78,6 +122,12 @@ public class Menu
         return userString;
     }
 
+    /**
+     * Prompts user for city, updated with user input correctness i.e. no numbers allowed in a city name
+     * @author Victor Ly
+     * @return String city
+     * @since (2/25) Commit 1af2ae19464a6d5da0a4385f95aad68b0dfe4029
+     */
     public static String prompt_City()
     {
         boolean invalidInput;
@@ -93,6 +143,12 @@ public class Menu
         return userString;
     }
 
+    /**
+     * Prompts user for state, updated with user input correctness i.e. no numbers allowed in a state name
+     * @author Victor Ly
+     * @return String state
+     * @since (2/25) Commit 1af2ae19464a6d5da0a4385f95aad68b0dfe4029
+     */
     public static String prompt_State()
     {
         boolean invalidInput;
@@ -108,6 +164,12 @@ public class Menu
         return userString;
     }
 
+    /**
+     * Prompts user for zip, updated with user input correctness i.e. no letters allowed in a zip
+     * @author Victor Ly
+     * @return String zip
+     * @since (2/25) Commit 1af2ae19464a6d5da0a4385f95aad68b0dfe4029
+     */
     public static String prompt_Zip()
     {
         boolean invalidInput;
@@ -123,6 +185,12 @@ public class Menu
         return userString;
     }
 
+    /**
+     * Prompts user for phone, updated with user input correctness i.e. no letters allowed in a phone number
+     * @author Victor Ly
+     * @return String telephone
+     * @since (2/25) Commit 1af2ae19464a6d5da0a4385f95aad68b0dfe4029
+     */
     public static String prompt_Telephone()
     {
         boolean invalidInput;
@@ -138,6 +206,12 @@ public class Menu
         return userString;
     }
 
+    /**
+     * Prompts user for email, both numbers and letters allowed in a valid email address
+     * @author Victor Ly
+     * @return String email
+     * @since (2/25) Commit 1af2ae19464a6d5da0a4385f95aad68b0dfe4029
+     */
     public static String prompt_Email()
     {
         System.out.println("Email: ");
@@ -145,6 +219,4 @@ public class Menu
         return userString;
     }
 }
-
-
 
